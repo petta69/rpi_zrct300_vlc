f2c85959ce1160d84f04561acf80fa3606f06fb6
Peter <43385830+petta69@users.noreply.github.com>
2024-05-27 11:44:17 +0200

    Update start_controller.sh

--- start_controller.sh
+++ start_controller.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -x
 
+export CONTROLLER_HOME="REPLACE"
+
+VIRTUAL_ENV="$CONTROLLER_HOME/.venv"
+export VIRTUAL_ENV
+
 ## Make sure we use the display of logged in user
 export DISPLAY=:0
 
-export CONTROLLER_HOME=$PWD
-VIRTUAL_ENV="$CONTROLLER_HOME/.venv"
-export VIRTUAL_ENV
 ## Define default config file for streamdeck
 export STREAMDECK_UI_CONFIG="$CONTROLLER_HOME/streamdeck_ui_export.json"
 
